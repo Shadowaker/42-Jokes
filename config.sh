de1dad929b0b9dc574f8865e7c0c5fd7c74d8a20
root <root@Shadan.localdomain>
2022-01-16 14:48:25 +0100

    Forgotten python for a moment there

--- config.sh
+++ config.sh
@@ -1,8 +1,8 @@
 
-alias clear=cd
-alias cd=bash alert.sh
-alias pwd=bash pewpew.sh
-alias ls=bash singing.sh && python3 owned.py
-alias cat=bash rick.sh
+alias clear="cd .."
+alias cd="bash ./alert.sh"
+alias pwd="bash ./pewpew.sh"
+alias ls="bash ./singing.sh && python3 owned.py"
+alias cat="bash ./rick.sh"
 
 echo "All set"
--- owned.py
+++ owned.py
@@ -2,6 +2,6 @@ import random
 import time
 
 while 1:
-	print("Have I been owned...? ")
-	print("AHAHAHA") * random.choice(range(0, 10))
-	time.sleep(0.5)
+    string = "AH" * random.choice(range(1, 11))
+    print(f"Have I been owned...? {string}")
+    time.sleep(0.5)
