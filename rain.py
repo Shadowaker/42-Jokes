72b63c74c1b1cabb1fb5213918f04960e1ba69b3
Daniele Ridolfo <shadan@Shadan.localdomain>
2022-11-17 20:58:04 +0100

    rain updated

--- rain.py
+++ rain.py
@@ -5,36 +5,44 @@ import time
 import random
 
 COLS, LINES = shutil.get_terminal_size()
-HIDE = "\x1b[?25l"
+HIDE = "\x1b[?25l\x1b[s"
+
+try:
+    WORD = sys.argv[1]
+except IndexError:
+    WORD = "\033[95mQUARANTADUE\033[0m"
+
 
 def cascade_first(lines):
     for x in lines:
-        sys.stdout.write(HIDE + x + "\n")
+        sys.stdout.write(x + "\n")
         sys.stdout.flush()
         time.sleep(0.05)
     
+
 def form():
-    try:
-        string =  " " * random.choice(range(1, COLS - len(sys.argv[1]))) + sys.argv[1]
-    except IndexError:
-        string = f"Have I been owned...?"
-        string += "AH" * random.choice(range(1, 22))
+    string =  " " * random.choice(range(1, COLS - len(WORD))) + WORD
     return string
 
+
 def main():
-    
     os.system("clear")
+    print(HIDE)
     while True:
         lines = []
-        for x in range(0, LINES - 1):
+        for x in range(0, LINES - 2):
             lines.append(form())
-        cascade_first(lines)
+        try:
+            cascade_first(lines)
+        except KeyboardInterrupt:
+            raise KeyboardInterrupt
         os.system("clear")
-
-    print("\x1b[u\x1b[?25h")
-        
 
 try:
     main()
 except KeyboardInterrupt:
-    pass
+    WORD = "MADONNE"
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\x1b[2J\x1b[u\x1b[?25h")
