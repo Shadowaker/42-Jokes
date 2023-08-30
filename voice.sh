ddb9880e00780e483e8487e624808bdd27177721
Giovanni Panico <giovannipanico314@gmail.com>
2023-08-30 14:28:17 +0200

    voice update
    now voice accepts string as first parameter

--- voice.sh
+++ voice.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 pactl set-sink-mute @DEFAULT_SINK@ false
-pactl set-sink-volume @DEFAULT_SINK@ 100%
-curl https://www.google.com/speech-api/v1/synthesize\?text\=$1\&lang\=it\&client\=lr-language-tts --output .voice &&  cvlc .voice vlc://quit && rm .voice
+pactl set-sink-volume @DEFAULT_SINK@ 50%
+var=$(echo "$1" | sed 's/\ /%20/g' )
+echo https://www.google.com/speech-api/v1/synthesize\?text\=$var\&lang\=it\&client\=lr-language-tts
+curl https://www.google.com/speech-api/v1/synthesize\?text\=$var\&lang\=it\&client\=lr-language-tts --output .voice && cvlc .voice vlc://quit
+rm -f .voice
