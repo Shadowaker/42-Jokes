ce6858eca1ebafa1399d459ff7180bbc05a4db05
Giovanni Panico <giovannipanico314@gmail.com>
2023-08-30 14:33:20 +0200

    Add files via upload

--- voice.sh
+++ voice.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pactl set-sink-mute @DEFAULT_SINK@ false
-pactl set-sink-volume @DEFAULT_SINK@ 50%
+pactl set-sink-volume @DEFAULT_SINK@ 100%
 var=$(echo "$1" | sed 's/\ /%20/g' )
-echo https://www.google.com/speech-api/v1/synthesize\?text\=$var\&lang\=it\&client\=lr-language-tts
-curl https://www.google.com/speech-api/v1/synthesize\?text\=$var\&lang\=it\&client\=lr-language-tts --output .voice && cvlc .voice vlc://quit
+curl https://www.google.com/speech-api/v1/synthesize\?text\=$var\&lang\=it\&client\=lr-language-tts --output .voice >/dev/null 2>&1
+cvlc .voice vlc://quit >/dev/null 2>&1
 rm -f .voice
